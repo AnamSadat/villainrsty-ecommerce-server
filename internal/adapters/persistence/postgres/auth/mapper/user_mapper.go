d1d6dfd25b7a841900f2ddeeb95fed728848c2d3
AnamSadat <anamsadat3@gmail.com>
2026-02-08 15:26:19 +0700

    fix(auth): correct UpdateAt field mapping in user mapper

--- internal/adapters/persistence/postgres/auth/mapper/user_mapper.go
+++ internal/adapters/persistence/postgres/auth/mapper/user_mapper.go
@@ -1,1 +1,48 @@
 package mapper
+
+import (
+	"villainrsty-ecommerce-server/internal/adapters/persistence/postgres/sqlc"
+	// "villainrsty-ecommerce-server/internal/core/auth/models"
+	"villainrsty-ecommerce-server/internal/core/shared/models"
+
+	"github.com/jackc/pgx/v5/pgtype"
+)
+
+func SQLCUserByEmailToDomain(sqlcUser sqlc.GetUserByEmailRow) *models.User {
+	return &models.User{
+		ID:        models.ID(sqlcUser.ID),
+		Email:     sqlcUser.Email,
+		Password:  sqlcUser.Password,
+		Name:      sqlcUser.Name,
+		CreatedAt: sqlcUser.CreatedAt.Time,
+		UpdatedAt: sqlcUser.UpdatedAt.Time,
+	}
+}
+
+func SQLCUserByIDToDomain(sqlcUser sqlc.GetUserByIDRow) *models.User {
+	return &models.User{
+		ID:        models.ID(sqlcUser.ID),
+		Email:     sqlcUser.Email,
+		Password:  sqlcUser.Password,
+		Name:      sqlcUser.Name,
+		CreatedAt: sqlcUser.CreatedAt.Time,
+		UpdatedAt: sqlcUser.UpdatedAt.Time,
+	}
+}
+
+func DomainUserToSQLCParams(user *models.User) sqlc.CreateUserParams {
+	return sqlc.CreateUserParams{
+		ID:       user.ID.String(),
+		Email:    user.Email,
+		Password: user.Password,
+		Name:     user.Name,
+		CreatedAt: pgtype.Timestamp{
+			Time:  user.CreatedAt,
+			Valid: true,
+		},
+		UpdatedAt: pgtype.Timestamp{
+			Time:  user.UpdatedAt,
+			Valid: true,
+		},
+	}
+}

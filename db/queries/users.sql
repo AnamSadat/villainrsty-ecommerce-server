cfb61e82232c05120563769555916e0a43d997a4
AnamSadat <anamsadat3@gmail.com>
2026-02-08 15:26:24 +0700

    fix(db): correct update_at parameter in CreateUser query

--- db/queries/users.sql
+++ db/queries/users.sql
@@ -1,2 +1,25 @@
--- name: ListUsers :many
-SELECT * FROM users;
+-- name: GetUserByEmail :one
+SELECT id, email, password, name, created_at, updated_at
+FROM users
+WHERE email = $1;
+
+-- name: GetUserByID :one
+SELECT id, email, password, name, created_at, updated_at
+FROM users
+WHERE id = $1;
+
+-- name: CreateUser :exec
+INSERT INTO users (id, email, password, name, created_at, updated_at)
+VALUES ($1, $2, $3, $4, $5, $6);
+
+-- name: UpdateUser :exec
+UPDATE users
+SET email = $2, password = $3, name = $4, updated_at = $5
+WHERE id = $1;
+
+-- name: DeleteUser :exec
+DELETE FROM users
+WHERE id = $1;
+
+-- name: UserExists :one
+SELECT EXISTS(SELECT 1 FROM users WHERE email = $1);

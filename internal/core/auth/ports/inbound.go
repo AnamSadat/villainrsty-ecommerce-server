d42c29c956971d01fb95e9971a9686a727fa521f
AnamSadat <anamsadat3@gmail.com>
2026-02-17 18:14:08 +0700

    feat(ports): update AuthService interface for refresh token

--- internal/core/auth/ports/inbound.go
+++ internal/core/auth/ports/inbound.go
@@ -6,9 +6,16 @@ import (
 	"villainrsty-ecommerce-server/internal/core/shared/models"
 )
 
-type AuthService interface {
-	Login(ctx context.Context, email, password string) (*models.User, string, error)
-	Register(ctx context.Context, email, password, name string) (*models.User, error)
-	RefreshToken(ctx context.Context, refreshToken string) (string, error)
-	ValidateToken(ctx context.Context, token string) (*models.User, error)
-}
+type (
+	AuthService interface {
+		Login(ctx context.Context, email, password string, rememberMe bool) (*models.User, string, string, error)
+		Register(ctx context.Context, email, password, name string) (*models.User, error)
+		RefreshToken(ctx context.Context, refreshToken string) (string, string, error)
+		ValidateToken(ctx context.Context, token string) (*models.User, error)
+		Logout(ctx context.Context, refreshToken string) error
+	}
+
+	EmailSender interface {
+		SendPasswordReset(ctx context.Context, toEmail, resetLink string) error
+	}
+)
